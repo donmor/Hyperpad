ae1e1ab0e417fb04f2c0f0940bdfb0a0b148b69a
donmor3000 <donmor3000@hotmail.com>
2020-02-20 12:05:22 +0800

    sync

--- app/src/androidTest/java/top/donmor/hyperpad/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/top/donmor/hyperpad/ExampleInstrumentedTest.java
@@ -2,7 +2,7 @@ package top.donmor.hyperpad;
 
 import android.content.Context;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
@@ -15,7 +15,7 @@ import static org.junit.Assert.assertEquals;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class ExampleInstrumentedTest {
 	@Test
 	public void useAppContext() {
